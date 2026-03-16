20991d90ac3c840cc5f86a1ae4a22973723396a2
Kenneth Gasmen <kengasmen@gmail.com>
2026-03-16 23:10:18 +0800

    feat: add role attribute to user factory and create UserSeeder for seeding users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Kenth',
+            'email' => 'ken@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'John the instructor',
+            'email' => 'instructor@example.com',
+            'role' => 'instructor'
+        ]);
+
+        User::factory()->create([
+            'name' => 'tyler the creator',
+            'email' => 'admin@example.com',
+            'role' => 'admin'
+        ]);
+
+        User::factory()->count(10)->create();
+
+        User::factory()->count(10)->create(
+            ['role' => 'instructor']
+        );
+    }
+}
